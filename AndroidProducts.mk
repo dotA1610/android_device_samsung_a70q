60eae6f433824eb9b79ed727173769caf9c1bcfb
dotA1610 <aswinsanker08@gmail.com>
2025-02-09 01:59:42 +0800

    welcome pixelage pt3

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a70q.mk
+    $(LOCAL_DIR)/pixelage_a70q.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a70q-user \
-    lineage_a70q-userdebug \
-    lineage_a70q-eng
+    pixelage_a70q-user \
+    pixelage_a70q-userdebug \
+    pixelage_a70q-eng
